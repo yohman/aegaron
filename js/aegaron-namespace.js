08a208a047cf3db039a98a9b8bc025865660d1e7
Yoh Kawano <yohman@gmail.com>
2015-10-02 18:10:53 -0700

    v1.1
    Satellite update

--- js/aegaron-namespace.js
+++ js/aegaron-namespace.js
@@ -274,7 +274,7 @@ var aegaron =
 			layer: new ol.layer.Tile({
 				mapid: 1,
 				source: new ol.source.XYZ({
-					url: 'http://marinus.library.ucla.edu:6080/arcgis/rest/services/Apollo/RoyalNecropolis/ImageServer/tile/{z}/{y}/{x}',
+					url: 'http://marinus.library.ucla.edu:6080/arcgis/rest/services/Apollo/RoyalNecropolis2/ImageServer/tile/{z}/{y}/{x}',
 					maxZoom: 23
 				}),
 				visible: true
@@ -342,7 +342,7 @@ var aegaron =
 			layer: new ol.layer.Tile({
 				mapid: 1,
 				source: new ol.source.XYZ({
-					url: 'http://marinus.library.ucla.edu:6080/arcgis/rest/services/Apollo/Uronart2/ImageServer/tile/{z}/{y}/{x}',
+					url: 'http://marinus.library.ucla.edu:6080/arcgis/rest/services/Apollo/Uronart3/ImageServer/tile/{z}/{y}/{x}',
 					maxZoom: 23
 				}),
 				visible: true
